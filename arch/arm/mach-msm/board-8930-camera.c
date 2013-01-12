3903efa8a6d026b73221caadf89f4c068405d7f5
albert <albert.lin@gigabyte.com>
2013-01-12 17:15:31 +0800

    change front camera face angle to correct.1
    Category: Camera
    Bug: None
    
    Change-Id: Ieda0eda2f53e920517bf8ac2d773d00c743881f5

--- arch/arm/mach-msm/board-8930-camera.c
+++ arch/arm/mach-msm/board-8930-camera.c
@@ -694,7 +694,7 @@ static struct msm_camera_csi_lane_params gc0339_csi_lane_params = {
 };
 
 static struct msm_camera_sensor_platform_info sensor_board_info_gc0339 = {
-	.mount_angle  = 90,
+	.mount_angle  = 0,
 	.cam_vreg = msm_8930_qrd_cam_vreg,
 	.num_vreg = ARRAY_SIZE(msm_8930_qrd_cam_vreg),
 	.gpio_conf = &msm_8930_front_cam_gpio_conf,
