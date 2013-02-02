89017a53c8095175315cca3b1d3056da6c266f18
harry.jiang <harry.jiang@gigabyte.com>
2013-02-02 20:41:18 +0800

    Add sensor gpio type and status.
    Category: psensor
    BUG: None
    
    Change-Id: I6d3920ef07d7bb3350c7cffcecd560a20f578b65

--- include/linux/iqs128.h
+++ include/linux/iqs128.h
@@ -2,10 +2,17 @@
 #ifndef _SQR128_H_
 #define _SQR128_H_
 
+enum {
+	TYPE_RF,
+	TYPE_WIFI,
+};
+
 struct iqs128_gpio_data {
 	int irq_gpio;
 	int irq;
 	int irqflags;
+	int status;
+	int type;
 };
 
 struct iqs128_platform_data {
