8d00f8106031bcddf06753500657b698a4fdaf30
brian.chen <brian31.chen@gigabyte.com>
2013-03-19 20:00:27 +0800

    To add bootlogo new func load_565rle_image_qrd_tablet_8x30.
    Category: LCM
    Bug: None
    
    Change-Id: I15c6c9b167b2544fc826639ed9e60114b7c96999

--- drivers/video/msm/logo.c
+++ drivers/video/msm/logo.c
@@ -23,6 +23,9 @@
 
 #include <linux/irq.h>
 #include <asm/system.h>
+#include <asm/cacheflush.h>
+
+#define INIT_IMAGE_FILE_PREFIX "/initlogo"
 
 #define fb_width(fb)	((fb)->var.xres)
 #define fb_height(fb)	((fb)->var.yres)
@@ -101,3 +104,67 @@ err_logo_close_file:
 	return err;
 }
 EXPORT_SYMBOL(load_565rle_image);
+
+int load_565rle_image_qrd_tablet_8x30(void)
+{
+	struct fb_info *info;
+	int fd, count, err = 0;
+	unsigned max;
+	unsigned short *data, *bits, *ptr;
+	unsigned char fname[32];
+
+	info = registered_fb[0];
+	if (!info) {
+		printk(KERN_WARNING "%s: Can not access framebuffer\n",
+			__func__);
+		return -ENODEV;
+	}
+
+	sprintf(fname, "%s_%dx%d", INIT_IMAGE_FILE_PREFIX,
+			fb_width(info), fb_height(info));
+	fd = sys_open(fname, O_RDONLY, 0);
+	if (fd < 0) {
+		printk(KERN_WARNING "%s: Can not open %s\n",
+			__func__, fname);
+		return -ENOENT;
+	}
+	count = sys_lseek(fd, (off_t)0, 2);
+	if (count <= 0) {
+		err = -EIO;
+		goto err_logo_close_file;
+	}
+	sys_lseek(fd, (off_t)0, 0);
+	data = kmalloc(count, GFP_KERNEL);
+	if (!data) {
+		printk(KERN_WARNING "%s: Can not alloc data\n", __func__);
+		err = -ENOMEM;
+		goto err_logo_close_file;
+	}
+	if (sys_read(fd, (char *)data, count) != count) {
+		err = -EIO;
+		goto err_logo_free_data;
+	}
+
+	max = fb_width(info) * fb_height(info);
+	ptr = data;
+	bits = (unsigned short *)(info->screen_base);
+	while (count > 3) {
+		unsigned n = ptr[0];
+		if (n > max)
+			break;
+		memset16(bits, ptr[1], n << 1);
+		bits += n;
+		max -= n;
+		ptr += 2;
+		count -= 4;
+	}
+	flush_cache_all();
+	outer_flush_all();
+
+err_logo_free_data:
+	kfree(data);
+err_logo_close_file:
+	sys_close(fd);
+	return err;
+}
+EXPORT_SYMBOL(load_565rle_image_qrd_tablet_8x30);
